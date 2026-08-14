5e045394681fe951470481e57b7768cd96b68aa7
Sercan <sercansimsek44@gmail.com>
2026-08-14 12:18:18 +0200

    Basic endpoint setup

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,19 @@ app.get("/api/notes", (req, res) => {
   res.send("you got 10 notes");
 });
 
+app.post("/api/notes", (req, res) => {
+  res.statusCode = 201;
+  res.json({ message: "Note created successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.statusCode = 200;
+  res.json({ message: "Note updated successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.statusCode = 200;
+  res.json({ message: "Note deleted successfully!" });
+});
+
 app.listen(PORT, () => console.log(`Server is running on the PORT: ${PORT}`));
