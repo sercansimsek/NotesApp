4b50acab2805f05daf6ec12042ed20664a3b9c6a
Sercan <sercansimsek44@gmail.com>
2026-08-14 12:11:59 +0200

    nodemon installed

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const PORT = 5001;
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-  res.send("you got 5 notes");
+  res.send("you got 10 notes");
 });
 
 app.listen(PORT, () => console.log(`Server is running on the PORT: ${PORT}`));
