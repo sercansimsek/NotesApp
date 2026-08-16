43a4ad7098af2fc3779b00625105960010cb997f
Sercan <sercansimsek44@gmail.com>
2026-08-16 15:37:33 +0200

    Home page created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { notesRouter } from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/notes", notesRouter);
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,46 @@
+import { useEffect, useState } from "react";
+import { Navbar } from "../components/Navbar";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { NoteCard } from "../components/NoteCard";
+
 export const Homepage = () => {
-  return <div className="text-rose italic">HomePage</div>;
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        setNotes(res.data);
+      } catch (error) {
+        console.error("Error fetching notes", error.message);
+        toast.error("Failed to load notes!");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {notes.length > 0 && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
