56f2f38563f46a358ae6b4c15b98bd8deeb134eb
Sercan <sercansimsek44@gmail.com>
2026-08-17 12:07:01 +0200

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,30 @@ import express from "express";
 import { notesRouter } from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
+import path from "path";
 
-const PORT = process.env.PORT;
 const app = express();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json());
 
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server is running on the PORT: ${PORT}`));
